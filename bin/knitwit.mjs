40e4af131517812b868620f650a1c58981f907b9
Karthik Ganeshram <karthik.ganeshram@fermyon.com>
2025-01-16 12:49:02 +0100

    allow configuring outworld and path when using the binA (#9)
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- bin/knitwit.mjs
+++ bin/knitwit.mjs
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
 import { knitWit } from "../dist/index.js";
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
 
-knitWit()
+
+// Parse command line arguments using yargs
+const argv = yargs(hideBin(process.argv))
+    .option('out-dir', {
+        type: 'string',
+        describe: 'Path to combined wit',
+        default: 'generated/wit/combined-wit'
+    })
+    .option('out-world', {
+        type: 'string',
+        describe: 'name of world in combined wit',
+        default: 'combined'
+    })
+    .argv;
+
+knitWit({ outDir: argv['out-dir'], outputWorld: argv['out-world'] });
